a875aba98b52dc46edc3d7917b5cfdbf8981c0a6
Wajahat Baloch <wajahatbaloch2002@gmail.com>
2024-03-04 14:33:23 +0500

    home-page-updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import { Box } from "@chakra-ui/react";
 
 import HeroSection from "@/Components/HeroSection";
-
 export default function Home() {
   return (
     <Box h={"100%"}>
-      <HeroSection />
+      <Box>
+        <HeroSection />
+      </Box>
+
+      {/* Cards */}
+      <Box height={"70vh"} width={"100%"} bg={"red"}></Box>
     </Box>
   );
 }
