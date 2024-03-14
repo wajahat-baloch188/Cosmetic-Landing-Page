59c7faa205e9f157d09be9f8eaa6fc26a498c27e
Wajahat Baloch <wajahatbaloch2002@gmail.com>
2024-03-14 11:29:48 +0500

    war3

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -155,7 +155,7 @@ export default function HeroSection() {
               {/* Left box with responsive image positioning */}
               <Box
                 pos="absolute" // Positioned absolutely
-                bottom={{ base: 0, md: -4, lg: -10 }} // Positioned at the bottom
+                bottom={{ base: 0, md: -4, lg: -10, xl: -18, "2xl": -30 }} // Positioned at the bottom
                 w={{ base: "50%", md: "50%", lg: "50%" }}
               >
                 <Image
